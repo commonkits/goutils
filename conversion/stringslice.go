b5016b0d998c1c11a01aee2d97e7ce954d8aa8dc
hengtian.antizhou <zj23qq@gmail.com>
2020-05-18 17:00:26 +0800

    conversion: string <-> slice

---
+++ conversion/stringslice.go
@@ -0,0 +1,23 @@
+package conversion
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func String2Slice(s string) (b []byte) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pbytes.Data = pstring.Data
+	pbytes.Len = pstring.Len
+	pbytes.Cap = pstring.Len
+	return
+}
+
+func Bytes2String(b []byte) (s string) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pstring.Data = pbytes.Data
+	pstring.Len = pbytes.Len
+	return
+}
